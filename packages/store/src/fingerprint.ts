05a943107750c43caf26dba13b5266e38ddc848b
Razzwan <razvanlomov@gmail.com>
2023-09-27 15:03:46 +0300

    fix(fingerprint): fix fingerprint getting

--- packages/store/src/fingerprint.ts
+++ packages/store/src/fingerprint.ts
@@ -1,12 +1,30 @@
+const FINGERPRINT_NAME = 'fingerprint';
+
 export const getFingerprint = async (): Promise<string> => {
 	try {
 		const {DeviceUUID} = await import('device-uuid');
-		return JSON.stringify({
+		let existingFingerprint: string | null = null;
+		try {
+			existingFingerprint = window.sessionStorage.getItem(FINGERPRINT_NAME);
+		} catch (err) {
+			console.error(err);
+		}
+		if (existingFingerprint) {
+			return existingFingerprint;
+		}
+		const newFingerprint = JSON.stringify({
 			browser: navigator.userAgent.replace(/[0-9/.]/g, ''),
 			device: new DeviceUUID().get(),
-			// userAgent: navigator.userAgent,
 		});
+
+		try {
+			window.sessionStorage.setItem(FINGERPRINT_NAME, newFingerprint);
+		} catch (err) {
+			console.error(err);
+		}
+		return newFingerprint;
 	} catch (e) {
+		console.error(e);
 		return Promise.resolve('');
 	}
 };
